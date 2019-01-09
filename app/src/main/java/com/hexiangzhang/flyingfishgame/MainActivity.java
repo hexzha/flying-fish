98ed35a760d070ac06fc77e28428ad0b5cd8ee09
Hexiang Zhang <sam10.zhang@gmail.com>
2019-01-08 21:45:02 -0600

    Have MainActivity use Flying Fish View

--- app/src/main/java/com/hexiangzhang/flyingfishgame/MainActivity.java
+++ app/src/main/java/com/hexiangzhang/flyingfishgame/MainActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private FlyingFishView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        gameView = new FlyingFishView(this);
+        setContentView(gameView);
     }
 }
