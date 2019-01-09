52ec648b38a55f2143ccc1fc94370e1f9e709cd5
Hexiang Zhang <sam10.zhang@gmail.com>
2019-01-08 23:02:21 -0600

    Reorganize SplashActivity

--- app/src/main/java/com/hexiangzhang/flyingfishgame/SplashActivity.java
+++ app/src/main/java/com/hexiangzhang/flyingfishgame/SplashActivity.java
@@ -1,8 +1,8 @@
 package com.hexiangzhang.flyingfishgame;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,11 +16,9 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 try {
                     sleep(5000);
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     e.printStackTrace();
-                }
-                finally {
+                } finally {
                     Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
                     startActivity(mainIntent);
                 }
