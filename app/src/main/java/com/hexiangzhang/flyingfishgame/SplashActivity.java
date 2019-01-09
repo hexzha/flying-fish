2713d79f3f8e519cd3bb1f401170965747af657c
Hexiang Zhang <sam10.zhang@gmail.com>
2019-01-08 21:23:34 -0600

    SplashAct 3 sec, Go to MainActivity.class

--- app/src/main/java/com/hexiangzhang/flyingfishgame/SplashActivity.java
+++ app/src/main/java/com/hexiangzhang/flyingfishgame/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.hexiangzhang.flyingfishgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,29 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                }
+            }
+        };
+
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
